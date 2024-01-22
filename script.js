79d763d07023e9d5d328203971bf1b83fda113f4
Khushi1524 <khushitiwari2415@gmail.com>
2024-01-22 19:28:58 +0530

    completed login form

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ function getNav(){
 }
 
 getNav();
+
+
+function getauth(){
+  fetch('./components/authentication.html')
+  .then(p => p.text())
+  .then(p => {
+    console.log(p)
+    document.body.innerHTML += p;
+  })
+}
+
+getauth();
+
+function closeauth(){
+  document.getElementById('auth2').style.display = 'none'
+}
+
+function openlogin(){
+
+document.getElementById('auth2').style.display = 'flex'
+document.getElementById('login').style.display = 'flex'
+document.getElementById('signup').style.display = 'none'
+alert('login')
+}
+
+function opensignup(){
+document.getElementById('auth2').style.display = 'flex'
+document.getElementById('login').style.display = 'none'
+document.getElementById('signup').style.display = 'flex'
+}
